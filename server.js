ccf92b1b48e4210cae78c349cbf1cad6908e9fa5
Luke Griffith <luketgriffith@gmail.com>
2016-02-15 14:31:09 -0500

    CORS

--- server.js
+++ server.js
@@ -32,6 +32,13 @@ app.use(session({
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true}));
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.engine('ejs', ejs_mate);
 app.set('view engine', 'ejs');
 
